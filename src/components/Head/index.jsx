760e96cf843455d351a38864ccc688ad9a748bff
Rino Nakagawara <nakagawara@reclick.co.jp>
2021-01-06 15:05:28 +0900

    feat: ヘッダーの実装

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import DeleteIcon from "@material-ui/icons/Delete";
+import "./index.css";
 
 const Head = () => {
-  return <div>head</div>;
+  return (
+    <div className="memo-head">
+      <div className="header">
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddCircleIcon />}
+        >
+          Add
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          startIcon={<DeleteIcon />}
+        >
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Head;
