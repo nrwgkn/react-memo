a268b48ed66e26a9cd6dfbd2e0458aabbba59d59
Rino Nakagawara <nakagawara@reclick.co.jp>
2021-01-06 15:20:34 +0900

    feat: ダミーデータを設定し、Contextを追加

--- src/components/Memo/index.jsx
+++ src/components/Memo/index.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "../Head";
 import Body from "../Body";
 import "./index.css";
 
+const dummyMemo = [
+  {
+    id: 1,
+    content: "dummyA",
+  },
+  {
+    id: 2,
+    content: "dummyB",
+  },
+  {
+    id: 3,
+    content: "dummyC",
+  },
+];
+
+export const Memos = React.createContext();
+
 const Memo = () => {
+  const [memos, setMemos] = useState(dummyMemo);
+
   return (
     <div className="memo">
-      <Head />
-      <Body />
+      <Memos.Provider value={[memos, setMemos]}>
+        <Head />
+        <Body />
+      </Memos.Provider>
     </div>
   );
 };
