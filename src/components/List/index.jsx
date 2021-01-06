f01294a3b1ac5ed73dbe71822e1fa1b15002d976
Rino Nakagawara <nakagawara@reclick.co.jp>
2021-01-06 16:03:23 +0900

    feat: memo一覧からクリックでmemoを選択できるようにする

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -5,11 +5,17 @@ const List = () => {
   const [memos, setMemos] = useContext(Memos);
   const [currentMemo, setCurrentMemo] = useContext(CurrentMemo);
 
+  const handleMemoClick = (id) => {
+    const clickedMemo = memos.find((memo) => memo.id === id);
+    setCurrentMemo(clickedMemo);
+  };
+
   const showMemoList = memos.map((memo) => {
     return (
       <li
         key={memo.id}
         className={currentMemo.id === memo.id ? "is-current" : ""}
+        onClick={() => handleMemoClick(memo.id)}
       >
         <p>{memo.content}</p>
       </li>
