7b46fce4167fcf2073fd35328648e4f94e89e37a
Rino Nakagawara <nakagawara@reclick.co.jp>
2021-01-06 14:55:08 +0900

    feat: Memoコンポーネントを作成

---
+++ src/components/Memo/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "../Head";
+import Body from "../Body";
+
+const Memo = () => {
+  return (
+    <div className="memo">
+      <Head />
+      <Body />
+    </div>
+  );
+};
+
+export default Memo;
